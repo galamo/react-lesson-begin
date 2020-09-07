843569c7ed7715b6c6a89ef078bd5eb543a5461c
chen roth <1@1.com>
2020-09-07 18:24:21 +0300

    set state spinner and user details

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,8 +8,9 @@ import AboutPage from './components/pages/about-page';
 import ConfigurationPage from './components/pages/configuration-page';
 import FavoritePage from './components/pages/favorite-page';
 import SearchResultPage from './components/pages/search-result-page';
-
+import axios from "axios"
 import { Link, Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import Spinner from 'react-bootstrap/Spinner';
 
 // jsx element
 
@@ -44,11 +45,32 @@ function RoutesConfiguration(props: { routes: Array<IRoute> }) {
 
 
 
+// let a = "";
+// function Mistake() {
+//     console.log(user.name.last)
+// }
 
-
+// Mistake();
+// a = { name: { last: "Roy" } };
+// Mistake();
 
 
 function NavBarApp() {
+
+    const [userDetails, setUserDetails] = useState(null)
+
+    async function getUserApi() {
+        const { data } = await axios.get("https://randomuser.me/api/?results=1")
+        const user = data.results[0]
+        setUserDetails(user)
+    }
+
+
+    useEffect(() => {
+        getUserApi()
+    }, [])
+
+    // if (!userDetails) return null
     return (<Navbar bg="light" expand="lg">
         <Navbar.Brand href="#home">Movies Api</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -58,14 +80,15 @@ function NavBarApp() {
                     const { path, name } = route;
                     return <Link to={path}> {name} </Link>
                 })}
-                {/* <Link to="/home"> Home </Link>
-                <Link to="/configuration"> Configuration </Link>
-                <Link to="/about"> About </Link>
-                <Link to="/ugly"> ugly </Link> */}
             </Nav>
         </Navbar.Collapse>
+
+        {userDetails ? <UserDetails user={userDetails} /> : < Spinner animation="border" role="status"> </Spinner>}
     </Navbar>)
 }
 
+function UserDetails(props: any) {
+    return <div> {props?.user?.name?.last} </div>
+}
 
 export default App;
